7c6d5ae80f00d032ac5a8e2ccd3941160a15bc52
Aryan Garg <gargaryan82000@gmail.com>
2023-08-15 11:08:34 +0530

    Edited The Service and Controller Files By Introducing Dependency Injection

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,35 @@
 import { Controller, Get, Post, Patch, Delete, Req, Param} from '@nestjs/common';
 import {Request} from 'express';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  // private userService;
+
+  // ! Dependency Injection
+  constructor(private userService: UserService){}
+
+  //! Normal way of importing
+  // constructor(userService: UserService){
+  //   // this.userService = userService;
+  //   this.userService = new UserService();
+  // }
   @Get()
   getUsers() {
-    // return userService.get();
+    return this.userService.get();
     // return { name: 'Aryan', email: 'gargaryan83000@gmail.com' };
   }
 
   @Post()
   store(@Req() request: Request) {
-    return request.body;
+    // return request.body;
+    return this.userService.create(request);
   }
 
   @Patch('/:userId')
   update(@Req() request: Request) {
-    return request.body;
+    // return request.body;
+    return this.userService.update(request);
   }
 
   @Get('/:userId')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
-// It is a provider that can inject dependencies. It can inject dependencies through constructor.   
+// It is a provider that can inject dependencies. It can inject dependencies through constructor.
 @Injectable()
 export class UserService {
-    get(){
-        return { name: 'Aryan', email: 'gargaryan83000@gmail.com' };
-    }
+  get() {
+    return { name: 'Aryan', email: 'gargaryan83000@gmail.com' };
+  }
+
+  create(req: Request) {
+    return req.body;
+  }
+
+  update()
 }
